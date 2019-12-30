fd57f733a631b6488d0c080819a45253d2dc820b
Rituparna Mahanty <ritumahanty216@gmail.com>
2019-12-30 13:50:10 +0530

    one line

---
+++ assignment1/div.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class div {
+
+	public static void main(String[] args) {
+	int a=50,b=3;
+	int c=a/b;
+	System.out.println(c);
+
+	}
+
+}
---
+++ assignment1/table.java
@@ -0,0 +1,14 @@
+package assignment1;
+
+public class table {
+	public static void main(String args[])
+	{
+		int n=8;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n+"*"+i+" = "+n*i);
+			
+			}
+	}
+	
+}
